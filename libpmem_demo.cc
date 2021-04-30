437c14f15a84616200ba9f51e38fbf015df10da4
KinderRiven <1339764596@qq.com>
2021-04-30 15:35:13 +0800

    push by hsk

--- libpmem_demo.cc
+++ libpmem_demo.cc
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-04-30 13:55:45
- * @LastEditTime: 2021-04-30 15:32:19
+ * @LastEditTime: 2021-04-30 15:35:09
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /pmdk-demo/libpmem_demo.cc
@@ -71,6 +71,8 @@ static void random_write(worker_context_t* context)
     uint32_t _bs = context->bs;
     uint32_t _skip = _bs * 4;
     uint64_t _src = (uint64_t)malloc(_bs);
+
+    printf("[%d][bs:%d][loop:%d][size:%.2fMB]\n", context->thread_id, _bs, g_num_loop, 1.0 * context->size / (1024UL * 1024));
 
     Timer _timer;
     _timer.Start();
