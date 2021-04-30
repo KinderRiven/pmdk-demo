8d4b4eceaf57e4e0fc7c46ef76267454f2308096
KinderRiven <1339764596@qq.com>
2021-04-30 18:39:28 +0800

    push by hsk

--- run_test.sh
+++ run_test.sh
@@ -1,15 +1,15 @@
 ###
  # @Author: your name
  # @Date: 2021-04-30 16:27:19
- # @LastEditTime: 2021-04-30 18:38:35
+ # @LastEditTime: 2021-04-30 18:39:25
  # @LastEditors: Please set LastEditors
  # @Description: In User Settings Edit
  # @FilePath: /pmdk-demo/run_test.sh
 ### 
 
-s_bs=(64 128 256)
-s_type=(rr rw sr sw)
-s_thread=(1 2 4)
+s_type=(rw)
+s_thread=(1 2 4 6 8 10 12)
+s_bs=(64 128 256 512 1024 2048 4096 8192 16384 32768 65536)
 
 for ((i=0; i<${#s_type[*]}; i+=1))
 do
